50e6a697044fd8a0128d05788bf0a0eb0a2311ca
vagrant <vagrant@homestead>
2016-11-24 15:34:26 +0000

    testing migration

---
+++ database/migrations/2016_11_24_153138_create_restos.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestos extends Migration
+{
+     /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps('created');
+            $table->timestamps('modified');
+            $table->string('name');
+            $table->integer('civic#')->unsigned();
+            $table->string('street');
+            $table->string('city');
+            $table->string('postal_code');
+            //$table->string('genre');
+            $table->string('price');
+            $table->decimal('longitude',9,6);
+            $table->decimal('latitude',9,6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restos');
+    }
+}
