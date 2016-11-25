743e4db68b9868fdf6cb81062cc66783d45ff246
vagrant <vagrant@homestead>
2016-11-25 21:21:32 +0000

    generated models skeletons

---
+++ app/Address.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    //Indicates that the model is not timestamped.
+    public $timestamps=false;
+    
+}
