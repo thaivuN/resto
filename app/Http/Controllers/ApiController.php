c22ddecdf8126d4f118a06156d3402721f4c800a
vagrant <vagrant@homestead>
2016-11-26 19:31:18 +0000

    created ApiController class for the RESTful API, will be implemented later

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * RESTful API class
+ * To be implemented later
+ */
+class ApiController extends Controller
+{
+    //
+}
