7eff1f9c70c3deca52fe4762bbba6346d91951f3
vagrant <vagrant@homestead>
2016-11-30 03:30:43 +0000

    got redirection logic done for GeoController, will continue implementation

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\GeoRepository;
+use App\Repositories\SearchRepository;
 use Illuminate\Http\Request;
 
 class GeoController extends Controller
 {
+    protected $georepo;
+    protected $searcher;
+    
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(GeoRepository $georepo, SearchRepository $seacher)
     {
         //$this->middleware('auth');
+        $this->georepo = $georepo;
+        $this->searcher = $seacher;
     }
 
     /**
@@ -23,20 +30,61 @@ class GeoController extends Controller
      */
     public function index(Request $request)
     {
-        if ($request->isMethod("post")){
+        if($request->isMethod("get")){
+            if($request->session()->has("latitude") && $request->session()->has("longitude"))
+            {
+                  
+                $lat = $request->session()->get('latitude');
+                $long = $request->session()->get('longitude');
+                
+                
+            }
+            else
+            {
+                return redirect("/home");
+            }
+        }
+        elseif ($request->isMethod("post")){
             $errorcode = $request->input("error");
             
-            if ($errorcode === 0){
+            if ($errorcode == 0){
                 $lat = $request->input("latitude");
                 $long = $request->input("longitude");
+                
+                
+                
             }
-            else{
+            else if ($errorcode == 6){
+                $postal = $this->georepo->GetGeocodingSearchResults($request->input('postal'));
+                
+                if (is_array($postal) && is_numeric($postal['latitude']) && is_numeric($postal['longitude'])){
+                    //The postal code is valid
+                    
+                    $lat = $postal['latitude'];
+                    $long = $postal['longitude'];
+                    
+                    
+                }
+                else{
+                    //The given address/postal code was not good enough
+                    //TO DO: Instead of redirecting to home, set default address;
+                    
+                    return redirect("/home");
+                     
+                }
+                
                 
             }
             
         }
+        
+        //$addresses = $this->searcher->getRestoAddressesNear($lat, $long);
         return view('geo.search');
+        //return view('geo.search', ['addresses' => $addresses,]);
 
     }
     
+    public function store(Request $request){
+        
+    }
 }
