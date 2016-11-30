13bec8b5764386dc2d3d79166cfca35e3d509930
vagrant <vagrant@homestead>
2016-11-30 20:28:56 +0000

    saving before screwing up everything

--- resources/views/geo/resto.blade.php
+++ resources/views/geo/resto.blade.php
@@ -1,8 +1,9 @@
-<?php
+@extends('layouts.app')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@section('content')
 
+<h1>Hello</h1>
+
+<p>To Do</p>
+
+@endsection
