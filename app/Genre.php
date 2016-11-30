361ba2a037b579330da20fef31a819172edb4112
vagrant <vagrant@homestead>
2016-11-30 20:44:40 +0000

    faker seeding working

--- app/Genre.php
+++ app/Genre.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
+    public $timestamps=false;
     /**
      * Gets all the restos that are under a particular genre.
      * @return array
