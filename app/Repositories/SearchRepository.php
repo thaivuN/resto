bffda87d13555a017a8b2a938bc5e513a17f22fa
vagrant <vagrant@homestead>
2016-12-02 16:47:31 +0000

    fixed the GetNearResto and added the usercanedit function to the user model

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use DB;
 use App\Address;
 
 /**
--- app/Resto.php
+++ app/Resto.php
@@ -4,40 +4,44 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Resto extends Model
-{
-    
-    protected $fillable = ['name', 'description' , 'price', 'phone#', 'email', 'user_id', 'genre_id'];
-    
+class Resto extends Model {
+
+    protected $fillable = ['name', 'description', 'price', 'phone#', 'email', 'user_id', 'genre_id'];
+
     /**
      * Gets all the reviews of the resto.
      * @return array
      */
-    public function reviews(){
+    public function reviews() {
         return $this->hasMany('App\Review');
     }
-    
+
     /**
      * Gets all the resto's genre.
      * @return array
      */
-    public function genres(){
+    public function genres() {
         return $this->belongsTo('App\Genre');
     }
-    
+
     /**
      * Gets the user that created the resto record.
      * @return User
      */
-    public function user(){
+    public function user() {
         return $this->belongsTo('App\User');
     }
-    
+
     /**
      * Gets all the locations of the resto.
      * @return array
      */
-    public function addresses(){
+    public function addresses() {
         return $this->hasMany('App\Address');
     }
+
+    public function userCanEdit(User $user) {
+        return $user->id === $this->user_id;
+    }
+
 }
