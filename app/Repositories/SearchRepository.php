46427f01f8e58fd80c801e500824fc1777bc33dc
vagrant <vagrant@homestead>
2016-12-02 16:19:38 +0000

    modified the get near resto logic

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -12,17 +12,20 @@ class SearchRepository {
 
     public function getRestoAddressesNear($latitude, $longitude, $radius = 50) {
 
-        $addresses = Address::selectRaw('( 6371 * acos( cos( radians(?) ) *
-          cos( radians( latitude ) )
-         * cos( radians( longitude ) - radians(?))
-          + sin( radians(?) ) *
-          sin( radians(latitude ) ) )
-          ) AS distance', [$latitude, $longitude, $latitude])
-          ->whereRaw("'distance' < ? ", [$radius])
-          ->orderBy('distance')
-          ->get();
-        
-        
+        $distances = Address::select('addresses.*')
+                ->selectRaw('( 6371 * acos( cos( radians(?) ) *
+            cos( radians( latitude ) )
+            * cos( radians( longitude ) - radians(?))
+            + sin( radians(?) ) *
+            sin( radians(latitude ) ) )
+          ) AS distance', [$latitude, $longitude, $latitude]);
+
+        $addresses = DB::table(DB::raw("({$distances->toSql()}) as restodistance"))
+                ->mergeBindings($distances->getQuery())
+                ->whereRaw("distance < ? ", [$radius])
+                ->orderBy('distance')
+                ->get();
+
         return $addresses;
     }
 
