c6b681d14bb39d21de99e4b8bfec5586fa501cea
vagrant <vagrant@homestead>
2016-12-04 00:53:52 +0000

    added ReviewController

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Resto;
+use App\Review;
 
 class ReviewController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
+    public function store(Request $request, $id){
+        $resto=Resto::find($id);
+        $review = new Review(['title' => $request->title, 'content' => $request->content, 'rating' => $request->rating]);
+        $resto->reviews()->save($review);
+        return redirect()->back();
+    }
+    
+    public function delete(Request $request, $id){
+        $review = Review::find($id);
+        $review->delete();
+        return redirect()->back();
+    }
 }
