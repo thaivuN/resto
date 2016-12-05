180f6c2368df1386f2891115dd03620085a1fa09
vagrant <vagrant@homestead>
2016-12-05 21:28:56 +0000

    Add style

---
+++ resources/views/home/index2.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                
+                <div class="panel-heading">
+                    <h3>Put in a Postal Code to find nearby restaurants</h3>
+                </div>
+
+                <div class="panel-body">
+    <!-- Geolocation -->
+     <form action="/geo" method="POST" class="form-horizontal" id="hiddenForm">
+         {{ csrf_field() }}
+         <!-- Postal code -->
+         <div class="form-group{{ $errors->has('postal') ? ' has-error' : '' }}">
+            <label for="postal" class="col-sm-3 control-label">Postal Code</label>
+            <div class="col-sm-6">
+               <input type="text" name="postal" id="postal" class="form-control" value="{{ old('postal') }}">
+               @if ($errors->has('postal'))
+                  <span class="help-block">
+                           <strong>{{ $errors->first('postal') }}</strong>
+                  </span>
+                @endif
+            </div>
+            
+         </div>
+         <!-- all the hidden fields -->
+         <input type="hidden" name="latitude"/>
+         <input type="hidden" name="longitude"/>
+         <input type="hidden" name="error"/>
+         <!-- submit Button -->
+         <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+               <button type="submit" class="btn btn-default">
+                   <i class="fa fa-btn fa-plus"></i>Submit
+                </button>
+            </div>
+         </div>
+     </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('js')
+    <script src="/js/geo.js"></script>
+@endsection
