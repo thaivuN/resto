53c102593a114a6b0f3906c9fb660395dee21322
vagrant <vagrant@homestead>
2016-12-05 22:59:42 +0000

    Add index style, incomplete

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,8 +2,41 @@
 @section('content')
 
 
-<div class="container">
-    MILK
+<div class="container parent_index">
+    <div>
+        <h1><span>Foody, </span>we help you find good food.</h1>
+
+        <!-- Geolocation -->
+        <form action="/geo" method="POST" class="form-horizontal" id="hiddenForm" style="margin-top: 5%;">
+            {{ csrf_field() }}
+
+            <!-- Postal code -->
+            <div class="input-group col-xs-3 {{ $errors->has('postal') ? ' has-error' : '' }}">
+                <input type="text" name="postal" id="postal" class="form-control index_input input-lg" value="{{ old('postal') }}" maxlength="7" placeholder="Postal Code">
+                @if ($errors->has('postal'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('postal') }}</strong>
+                </span>
+                @endif
+
+                <!-- submit Button -->
+                <span class="input-group-btn">
+                    <button type="submit" class="btn btn-warning btn-lg">
+                        Start
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </button>
+                </span>
+
+            </div>
+
+
+
+            <!-- all the hidden fields -->
+            <input type="hidden" name="latitude"/>
+            <input type="hidden" name="longitude"/>
+            <input type="hidden" name="error"/>
+        </form>
+    </div>
 </div>
 
 
@@ -11,5 +44,5 @@
 @endsection
 
 @section('js')
-    <script src="/js/geo.js"></script>
+<script src="/js/geo.js"></script>
 @endsection
--- resources/views/layouts/nav_index.php
+++ resources/views/layouts/nav_index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <!-- CSRF Token -->
-      <meta name="csrf-token" content="{{ csrf_token() }}">
-      <title>Resto Test</title>
-      <!-- Styles -->
-      <link href="/css/app.css" rel="stylesheet">
-      <link href="/css/custom.css" rel="stylesheet">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <!-- Scripts -->
-      <script>
-         window.Laravel = <?php echo json_encode([
-            'csrfToken' => csrf_token(),
-            ]); ?>
-      </script>
-   </head>
-   <body id="index_body">
-      <div>
-         <nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                <div class="navbar-header">
                   <!-- Collapsed Hamburger -->
@@ -95,10 +73,3 @@
                </div>
             </div>
          </nav>
-         @yield('content')
-      </div>
-      <!-- Scripts -->
-      <script src="/js/app.js"></script>
-      @yield('js')
-   </body>
-</html>
