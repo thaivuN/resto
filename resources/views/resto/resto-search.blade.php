4abbeafcbd5f6f3f7f74289d3546ffd9be7c9d9d
vagrant <vagrant@homestead>
2016-12-07 22:26:38 +0000

    Trying to push fixes

--- resources/views/resto/resto-search.blade.php
+++ resources/views/resto/resto-search.blade.php
@@ -56,59 +56,5 @@
 @else
     <strong>We're sorry, we do not have any restos for you.</strong>
 @endif
-{{--
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            Search Results
-        </div>
-
-        <div class="panel-body">
-            @if (count($restos) > 0)
-            <table class="table table-striped resto-table">
-
-                <!-- Table Headings -->
-                <thead>
-                <th>Restaurant Name</th>
-                <th>Price</th>
-                <th>Postal Code</th>
-                <th>Rating</th>
-                </thead>
-
-                <!-- Table Body -->
-                <tbody>
-                @foreach ($restos as $resto)
-                    <tr>
-                        <!-- Resto Name -
-                        <td class="table-text">
-                            <div><a href="{{url('/resto_info/'.$resto->id)}}">{{ $resto->name }}</a></div>
-                        </td>
-
-                        <td class="table-text">
-                            <div>
-                                @for($i = 0; $i < $resto->price; $i++)
-                                    {{'$'}}
-                                @endfor
-                            </div>
-                        </td>
-                        <td class="table-text">
-                            <div>{{$resto->postal_code}}</div>
-                        </td>
-                        
-                        <td class="table-text">
-                            <div>{{$ratings[$resto->id]}}</div>
-                        </td>
-                        
-                    </tr>
-                    -->
-                @endforeach
-                </tbody>
-            </table>
-            {!! $restos->render() !!}
-            @else
-            <strong>No Results</strong>
-            @endif
-        </div>
-    </div>
-    --}}
 </div>
 @endsection
