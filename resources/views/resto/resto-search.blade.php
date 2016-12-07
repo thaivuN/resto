4d490506fc3391bb94c7e7b951f1f2b6e6bd40f2
vagrant <vagrant@homestead>
2016-12-07 22:22:37 +0000

    Work on search

--- resources/views/resto/resto-search.blade.php
+++ resources/views/resto/resto-search.blade.php
@@ -4,6 +4,59 @@
 
 
 <div class="container">
+
+@if(count($restos) > 0)
+    
+    @if($loop->index % 3 === 0)
+        <div class="row"></div>
+    @endif
+
+    <div class="panel panel-primary col-md-4 col-xs-12">
+        <div class="panel-heading"><a href="{{url('/resto_info/'.$resto->id)}}">{{ $resto->name }}</a></div>
+        <div class="panel-body">
+            {{-- Image --}}
+        </div>
+
+        <div class="panel-body">
+            {{-- general info --}}
+
+            <ul class="list-inline">
+                <li>
+                    {{-- Pricing --}}
+                    @for($i = 0; $i < $resto->price; $i++)
+                        {{'$'}}
+                    @endfor
+                </li>
+
+                <li>
+                    {{-- Rating --}}
+                    <div class="rating">
+                    @for($i = 0 ; $i < 5 ; $i++)
+                        @if($ratings[$resto->id] - $1 > 1)
+                            <i class="glyphicon glyphicon-star"></i>
+                        @elseif($ratings[$resto->id] - $1 < 0)
+                            <i class="glyphicon glyphicon-star-empty"></i>
+                        @else
+                            <i class="glyphicon glyphicon-star half"></i>
+                        @endif
+                    @endfor
+                    </div>
+
+                </li>
+                <li></li>
+            </ul>
+
+        </div>
+    </div>
+
+    @if($loop->index % 3 === 2)
+        </div>
+    @endif
+
+@else
+    <strong>We're sorry, we do not have any restos for you.</strong>
+@endif
+
     <div class="panel panel-default">
         <div class="panel-heading">
             Search Results
@@ -25,7 +78,7 @@
                 <tbody>
                 @foreach ($restos as $resto)
                     <tr>
-                        <!-- Resto Name -->
+                        <!-- Resto Name -
                         <td class="table-text">
                             <div><a href="{{url('/resto_info/'.$resto->id)}}">{{ $resto->name }}</a></div>
                         </td>
@@ -46,6 +99,7 @@
                         </td>
                         
                     </tr>
+                    -->
                 @endforeach
                 </tbody>
             </table>
@@ -56,6 +110,4 @@
         </div>
     </div>
 </div>
-
-
 @endsection
