d0ff0d2eba0c6270f6fa4894f810efdde7a3877a
vagrant <vagrant@homestead>
2016-12-08 17:26:20 +0000

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
       <link href="/css/app.css" rel="stylesheet">
       <link href="/css/custom.css" rel="stylesheet">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
       <!-- Scripts -->
       <script>
          window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
             ]); ?>
       </script>
+      <script src="https://use.fontawesome.com/d16d023904.js"></script>
       
    </head>
    <body id="index_body">
