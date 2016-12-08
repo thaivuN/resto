4c6d8062be6ac6c63cbd2f4be5f2692131b6f137
vagrant <vagrant@homestead>
2016-12-08 21:37:42 +0000

    fixed validation for create resto from the REST API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -42,16 +42,16 @@ class ApiController extends Controller {
         }
         return response()->json($reviews, 200);
     }
-    
+
     /**
      * Returning the Resto model based on an id
      * @param Request $request
      * @return json response
      */
-    public function getRestoDetails(Request $request){
+    public function getRestoDetails(Request $request) {
         $this->validate($request, ['id' => 'required|numeric']);
         $resto = Resto::find($request->id);
-        
+
         return response()->json($resto, 200);
     }
 
@@ -95,9 +95,9 @@ class ApiController extends Controller {
      */
     public function getNearbyRestos(Request $request) {
         $this->validate($request, ['lat' => 'required|numeric', 'long' => 'required|numeric']);
-        
+
         $restos = $this->searcher->getTenNearestResto($request->lat, $request->long);
-      
+
         return response()->json($restos, 200);
     }
 
@@ -110,73 +110,97 @@ class ApiController extends Controller {
     public function registerResto(Request $request) {
 
         $credentials = $request->only('email', 'password');
-       
+
         $valid = Auth::once($credentials);
         if (!$valid) {
             return response()->json(['error', 'invalid_credentials'], 401);
-        } else {
-            $this->validate($request, [
-                'name' => 'required|max:255',
-                'description' => 'present|max:5000',
-                'resto_email' => 'present|email|max:255',
-                'price' => 'required|numeric|min:1|max:5',
-                'phone' => 'present|max:255',
-                'civic_num' => 'required|numeric',
-                'street' => 'required|max:255',
-                'suite' => 'present|numeric',
-                'city' => 'required|max:255',
-                'country' => 'required|max:255',
-                'postal_code' => 'required|max:255',
-                'province' => 'required|max:255',
-                'link' => 'present|url|max:255',
-                'genre' => 'required|max:255',
-                'img' => 'present|url'
-            ]);
+        }
+        
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'description' => 'present|max:5000',
+            'resto_email' => 'present|email|max:255',
+            'price' => 'required|numeric|min:1|max:5',
+            'phone' => 'present|max:255',
+            'civic_num' => 'required|numeric',
+            'street' => 'required|max:255',
+            'suite' => 'present|numeric',
+            'city' => 'required|max:255',
+            'country' => 'required|max:255',
+            'postal_code' => 'required|max:255',
+            'province' => 'required|max:255',
+            'link' => 'present|url|max:255',
+            'genre' => 'required|max:255',
+            'img' => 'present|url'
+        ]);
 
-            $pairs = $this->georepo->GetGeocodingSearchResults($request->postal_code);
+        $pairs = $this->georepo->GetGeocodingSearchResults($request->postal_code);
 
-            $extraValidator = Validator::make(
-                            [
-                        'latitude' => $pairs['latitude'],
-                        'longitude' => $pairs['longitude']
-                            ], [
-                        'latitude' => 'required|numeric',
-                        'longitude' => 'required|numeric'
-            ]);
+        $extraValidator = Validator::make(
+                        [
+                    'latitude' => $pairs['latitude'],
+                    'longitude' => $pairs['longitude']
+                        ], [
+                    'latitude' => 'required|numeric',
+                    'longitude' => 'required|numeric'
+        ]);
 
-            if ($extraValidator->fails()) {
-                return response()->json(['error' => 'Postal Code does not lead to a real location'], 422);
-            }
+        if ($extraValidator->fails()) {
+            return response()->json(['error' => 'Postal Code does not lead to a real location'], 422);
+        }
+        
+        if ($this->validateUniqueResto($request, $pairs) == false){
+            return response()->json(['error' => 'The restaurant already exists'], 422);
+        }
 
-            
-            $resto = new Resto([
-                'name' => $request->name,
-                'latitude' => $pairs['latitude'],
-                'longitude' => $pairs['longitude']
-            ]);
-            $resto->description = $request->description;
-            $resto->email = $request->resto_email;
-            $resto->phone = $request->phone;
-            $resto->civic_num = $request->civic_num;
-            $resto->price = $request->price;
-            if (is_numeric($request->suite)) {
-                $resto->suite = $request->suite;
-            }
-            $resto->street = $request->street;
-            $resto->province = $request->province;
-            $resto->image_link = $request->img;
-            $resto->postal_code = $request->postal_code;
-            $resto->city = $request->city;
-            $resto->country = $request->country;
-            $resto->link = $request->link;
-            $genre = \App\Genre::firstOrCreate(['genre' => $request->genre]);
-            $resto->genre_id = $genre->id;
-            $user = User::where('email', $request->email)->first();
-            $resto->user_id = $user->id;
-            $resto->save();
 
-            return response()->json(['success' => 'The restaurant was successfully created'], 200);
-            
+        $resto = new Resto([
+            'name' => $request->name,
+            'latitude' => $pairs['latitude'],
+            'longitude' => $pairs['longitude']
+        ]);
+        $resto->description = $request->description;
+        $resto->email = $request->resto_email;
+        $resto->phone = $request->phone;
+        $resto->civic_num = $request->civic_num;
+        $resto->price = $request->price;
+        if (is_numeric($request->suite)) {
+            $resto->suite = $request->suite;
+        }
+        $resto->street = $request->street;
+        $resto->province = $request->province;
+        $resto->image_link = $request->img;
+        $resto->postal_code = $request->postal_code;
+        $resto->city = $request->city;
+        $resto->country = $request->country;
+        $resto->link = $request->link;
+        $genre = \App\Genre::firstOrCreate(['genre' => $request->genre]);
+        $resto->genre_id = $genre->id;
+        $user = User::where('email', $request->email)->first();
+        $resto->user_id = $user->id;
+        $resto->save();
+
+        return response()->json(['success' => 'The restaurant was successfully created'], 200);
+    }
+
+    /**
+     * Check if the combination of the longitude, latitude and name is unique in the database
+     * 
+     * @param Request $request
+     * @param array $pairs containing latitude and longitude keys
+     * @return boolean
+     */
+    private function validateUniqueResto(Request $request, $pairs) {
+        $exists = Resto::where('name', '=', $request->name)
+                ->where('latitude', '=', $pairs['latitude'])
+                ->where('longitude', '=', $pairs['longitude'])
+                ->count();
+
+        //there is a non-0 count, the resto is not unique 
+        if ($exists) {
+            return false;
+        } {
+            return true;
         }
     }
 
