b909d691ee50f3b5b6e6b09218e60404e5c79c57
vagrant <vagrant@homestead>
2016-12-08 19:14:17 +0000

    fixed the null ratings on GeoController

--- app/Http/Controllers/GeoController.php
+++ app/Http/Controllers/GeoController.php
@@ -10,20 +10,22 @@ use App\Repositories\SearchRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Controller used to handle the search of nearby restaurants
+ * 
+ * @author Thai-Vu Nguyen
+ */
+class GeoController extends Controller {
 
-class GeoController extends Controller
-{
     protected $georepo;
     protected $searcher;
-    
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(GeoRepository $georepo, SearchRepository $seacher)
-    {
-        //$this->middleware('auth');
+    public function __construct(GeoRepository $georepo, SearchRepository $seacher) {
         $this->georepo = $georepo;
         $this->searcher = $seacher;
     }
@@ -33,73 +35,59 @@ class GeoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
-    {
-        if($request->isMethod("get")){
-            if($request->session()->has("latitude") && $request->session()->has("longitude"))
-            {
-                 
+    public function index(Request $request) {
+        if ($request->isMethod("get")) {
+            if ($request->session()->has("latitude") && $request->session()->has("longitude")) {
                 $lat = $request->session()->get('latitude');
                 $long = $request->session()->get('longitude');
-                
+            } else {
+                //Check if user is authenticated, if so, use his postal code
+                if (Auth::check()) {
+                    $user = Auth::user();
+                    $postal = $this->georepo->GetGeocodingSearchResults($user->postal_code);
+                    $lat = $postal['latitude'];
+                    $long = $postal['longitude'];
+                } else {
+                    return redirect("/home"); //Go back to the form
+                }
             }
-            else
-            {
-                return redirect("/home");
-            }
-        }
-        elseif ($request->isMethod("post")){
+        } elseif ($request->isMethod("post")) {
             $errorcode = $request->input("error");
-            
-            if ($errorcode == 0){
+
+            if ($errorcode == 0) {
                 $lat = $request->input("latitude");
                 $long = $request->input("longitude");
-                
-                
-                
-            }
-            else{   
+            } else {
                 $postal = null;
-                try{
-                $postal = $this->georepo->GetGeocodingSearchResults($request->input('postal'));
-                }
-                catch(\ErrorException $e){
+                try {
+                    $postal = $this->georepo->GetGeocodingSearchResults($request->input('postal'));
+                } catch (\ErrorException $e) {
                     //Happens when user put in an empty string.
-                    //Since we do check
                 }
-                
-                $validator = Validator::make(['latitude' => $postal['latitude'], 'longitude' => $postal['longitude']], 
-                ['latitude' => 'required|numeric', 'longitude' => 'required|numeric']);
-                
-                if ($validator->fails()){
-                    //The given address/postal code was not good enough
-                    //TO DO: Instead of redirecting to home, set default address;
-                    
+
+                $validator = Validator::make(['latitude' => $postal['latitude'], 'longitude' => $postal['longitude']], ['latitude' => 'required|numeric', 'longitude' => 'required|numeric']);
+
+                if ($validator->fails()) {
+                    //The given postal code was not good enough
                     return redirect("/home");
-                     
                 }
-                
+
                 $lat = $postal['latitude'];
                 $long = $postal['longitude'];
-                
             }
-            
-            
         }
-        
+
         $request->session()->put("latitude", $lat);
         $request->session()->put("longitude", $long);
-        
+
         $restos = $this->searcher->getRestoAddressesNear($lat, $long);
-        foreach($restos as $resto){
+        $ratings = null;
+        foreach ($restos as $resto) {
             $ratings[$resto->id] = $this->searcher->getAverageRating($resto);
         }
-        //return view('geo.search');
-        //return view('geo.nearbyresto', ['restos' => $restos,]);
-        //return view('geo.nearbyresto')->with('restos',$restos)->with("ratings",$ratings);
-        return view('geo.nearbyresto')->with('restos',$restos)->with("ratings",$ratings);
 
+        return view('geo.nearbyresto')->with('restos', $restos)->with("ratings", $ratings);
     }
-   
-    
+
+
 }
