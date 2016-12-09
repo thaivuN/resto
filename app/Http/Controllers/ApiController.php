5148f876b64298ae4f455402762b133b41f46444
vagrant <vagrant@homestead>
2016-12-09 23:29:35 +0000

    test master

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -67,11 +67,11 @@ class ApiController extends Controller {
      */
     public function storeReviews(Request $request) {
 
-        //$credentials = $request->only('email', 'password');
-        //$temp = $request->only('email', 'password');
-        //var_dump($temp);
-        //$valid = ;
-        if (!Auth::once($request->only('email', 'password'))) {
+        $credentials = $request->only('email', 'password');
+        $temp = $request->all();
+        var_dump($temp->email);
+        $valid = Auth::once($credentials);
+        if (!$valid) {
             return response()->json(['error' => 'invalid_cr'], 401);
         } else {
             $this->validate($request, ['id' => 'required|numeric|exists:restos,id',
