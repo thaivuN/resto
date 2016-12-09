f8effbab27f517292d9310c8e77fb156f40002e4
vagrant <vagrant@homestead>
2016-12-09 17:00:33 +0000

    changed redirection

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/geo';
 
     /**
      * Create a new controller instance.
