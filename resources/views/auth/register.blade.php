322df57c1ce2ffdc1427e9065f2005ba41582151
vagrant <vagrant@homestead>
2016-12-09 20:29:27 +0000

    fsdfsd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('page-content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-2">
         <div class="text-center">
             <h2 class="custom-header">Register</h2>
         </div>
@@ -85,7 +85,7 @@
 
                 {{-- Button --}}
                 <div class="form-group" style="padding-top: 20px;">
-                    <div class="col-md-12">
+                    <div class="col-md-12" style="padding-top: 20px;">
                     <button type="submit" class="btn btn-warning">
                         Register
                     </button>
